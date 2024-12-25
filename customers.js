1c7e7a9e82ce77a96229eec3d015f23a486ba2ad
Pawan Samaraweera <psmsamaraweera@gmail.com>
2024-12-26 00:20:00 +0530

    fvghj

--- customers.js
+++ customers.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const customers = [
-        { id: 1, name: "John Doe", latitude: 6.9271, longitude: 79.8612 },
+        { id: 10, name: "John Doe", latitude: 6.9271, longitude: 79.8612 },
         { id: 2, name: "Jane Smith", latitude: 6.9147, longitude: 79.8487 },
         { id: 3, name: "Robert Brown", latitude: 6.9325, longitude: 79.8567 }
     ];
